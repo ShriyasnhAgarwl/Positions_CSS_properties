54280c04524b03465c111c154d33d9d919f95cb9
SHRIYANSH AGARWAL <shriyanshagar9026@gmail.com>
2023-08-16 16:48:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1> Positions</h1>
+      <div>Position :Static </div>
+      <div className="static">
+        Hello, there my name is shriyansh agarwal and i am working as a software
+        engineer at Invincible Ocean
+      </div>
+      <br></br>
+      <div>Position :Relative </div>
+      <div className="relative">
+        Hello, there my name is shriyansh agarwal and i am working as a software
+        engineer at Invincible Ocean
+      </div>
+      <br></br>
+      <div>Position :Fixed </div>
+      <div className="fixed">
+        Hello, there my name is shriyansh agarwal and i am working as a software
+        engineer at Invincible Ocean
+      </div>
+      <br></br>
+      <div>Position :Absolute </div>
+      <div className="absolute">
+        Hello, there my name is shriyansh agarwal and i am working as a software
+        engineer at Invincible Ocean
+      </div>
+    </div>
+  );
+}
